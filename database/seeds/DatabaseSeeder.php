875bd12978532d1228b9e714640fe078709054c5
SaifN97 <saifnarpali@gmail.com>
2020-12-23 17:20:14 +0530

    Added seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Laratube\Channel;
+use Laratube\Subscription;
+use Laratube\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //Users
+        $user1 = factory(User::class)->create([
+            'email' => 'john@doe.com'
+        ]);
+
+        $user2 = factory(User::class)->create([
+            'email' => 'jane@doe.com'
+        ]);
+
+
+        //Channels
+        $channel1 = factory(Channel::class)->create([
+            'user_id' => $user1->id
+        ]);
+
+        $channel2 = factory(Channel::class)->create([
+            'user_id' => $user2->id
+        ]);
+
+        //Subscriptions
+        $channel1->subscriptions()->create([
+            'user_id' => $user2->id
+        ]);
+
+        $channel2->subscriptions()->create([
+            'user_id' => $user1->id
+        ]);
+
+
+        //Test subs
+        factory(Subscription::class, 500)->create([
+            'channel_id' => $channel1->id
+        ]);
+
+        factory(Subscription::class, 500)->create([
+            'channel_id' => $channel2->id
+        ]);
     }
 }
