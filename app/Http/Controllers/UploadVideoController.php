e2e0ed5956b87a0e66f66440504a76e335a6de10
SaifN97 <saifnarpali@gmail.com>
2020-12-25 13:36:29 +0530

    Added setup for uploading video

---
+++ app/Http/Controllers/UploadVideoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Laratube\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laratube\Channel;
+
+class UploadVideoController extends Controller
+{
+    public function index(Channel $channel)
+    {
+        return view('channels.upload', compact('channel'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Laratube\Http\Controllers\UploadVideoController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,3 +23,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/channels', 'ChannelController');
 Route::resource('/channels/{channel}/subscriptions', 'SubscriptionController')->only(['store', 'destroy'])->middleware(['auth']);
+
+Route::get('/channels/{channel}/videos', [UploadVideoController::class, 'index'])->name('channel.upload');
