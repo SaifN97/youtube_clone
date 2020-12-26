3ec61b488843c3aab1c61d7e5ccee0b7dfac85ed
SaifN97 <saifnarpali@gmail.com>
2020-12-26 15:28:01 +0530

    Added video conversion for streaming with ffmpeg and job queues for processing

--- app/Jobs/Videos/ConvertForStreaming.php
+++ app/Jobs/Videos/ConvertForStreaming.php
@@ -2,12 +2,14 @@
 
 namespace Laratube\Jobs\Videos;
 
+use FFMpeg\Format\Video\X264;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Laratube\Video;
+use Pbmedia\LaravelFFMpeg\FFMpegFacade;
 
 class ConvertForStreaming implements ShouldQueue
 {
@@ -32,6 +34,16 @@ class ConvertForStreaming implements ShouldQueue
      */
     public function handle()
     {
-        echo 'converted';
+        $low = (new X264('aac'))->setKiloBitrate(100);
+        $med = (new X264('aac'))->setKiloBitrate(250);
+        $high = (new X264('aac'))->setKiloBitrate(500);
+
+        FFMpegFacade::fromDisk('local')
+            ->open($this->video->path)
+            ->exportForHLS()
+            ->addFormat($low)
+            ->addFormat($med)
+            ->addFormat($high)
+            ->save("public/videos/{$this->video->id}/{$this->video->id}.m3u8");
     }
 }
--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // Laratube\Providers\BroadcastServiceProvider::class,
         Laratube\Providers\EventServiceProvider::class,
         Laratube\Providers\RouteServiceProvider::class,
+        Pbmedia\LaravelFFMpeg\FFMpegServiceProvider::class,
 
     ],
 
@@ -225,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'FFMpeg' => Pbmedia\LaravelFFMpeg\FFMpegFacade::class
 
     ],
 
