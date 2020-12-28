baa78235f62aef9d8ebcad54d7d36db2f5fc4fb4
SaifN97 <saifnarpali@gmail.com>
2020-12-28 19:18:46 +0530

    Added pagination for comments

--- resources/js/components/comments.vue
+++ resources/js/components/comments.vue
@@ -34,9 +34,10 @@
             </div>
         </div>
     </div>
-    
+
     <div class="text-center">
-        <button class="btn btn-success">Load More</button>
+        <button v-if="comments.next_page_url" @click="fetchComments" class="btn btn-success">Load More</button>
+        <span v-else>No more comments to show :)</span>
     </div>
 </div>
 </template>
@@ -60,10 +61,17 @@ export default {
 
     methods: {
         fetchComments() {
-            axios.get(`/videos/${this.video.id}/comments`).then(({
-                data
-            }) => {
-                this.comments = data
+const url = this.comments.next_page_url ? this.comments.next_page_url : `/videos/${this.video.id}/comments` 
+
+            axios.get(url).then(({data}) => {
+                this.comments = {
+                    ...data,
+                    data: [
+                        ...this.comments.data,
+                        ...data.data
+                    ]
+                }
+
             })
         }
     }
