e5205037b26d3733e9026d03073ed74f9a7aaa9f
SaifN97 <saifnarpali@gmail.com>
2020-12-28 22:12:03 +0530

    Added front-end for displayin replies on comments

--- app/Comment.php
+++ app/Comment.php
@@ -7,9 +7,16 @@ class Comment extends Model
 {
     protected $with = ['user'];
 
+    protected $appends = ['repliesCount'];
+
     public function video()
     {
         return $this->belongsTo(Video::class);
+    }
+
+    public function getRepliesCountAttribute()
+    {
+        return $this->replies->count();
     }
 
     public function user()
